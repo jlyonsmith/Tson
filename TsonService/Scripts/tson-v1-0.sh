39beaaf30dc7b714e997cf24b958dc6e7baa25af
jlyonsmith <john@lyon-smith.org>
2014-06-02 18:20:35 -0700

    Fixing deploy scripts

--- TsonService/Scripts/tson-v1-0.sh
+++ TsonService/Scripts/tson-v1-0.sh
@@ -3,5 +3,5 @@
 # WARNING: Do not edit these files in Xamarin Studio as they will get BOM's 
 # which screws up bash shell scripts.
 #
-/usr/bin/mono /home/ubuntu/lib/FerryView.v1.0/FerryViewService.exe $*
+/usr/bin/mono /home/ubuntu/lib/Tson.v1.0/TsonService.exe $*
 
